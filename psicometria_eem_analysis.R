273d2b4b89394ccc76e5df2dd3686e7ea4b0c739
Nelson Hauck <65832166+hauck-nf@users.noreply.github.com>
2023-03-29 11:42:15 -0300

    Update psicometria_eem_analysis.R

--- psicometria_eem_analysis.R
+++ psicometria_eem_analysis.R
@@ -8,14 +8,33 @@ library(readxl)
 
 #dados e dicionário
 setwd("C:/Users/hauck/OneDrive/Main Drive/Manuscritos/Ariela - escala Vecchione/Data")
-em_bd<-read_xlsx("EMS - Banco integrado Coleta 2016.xlsx",1)
-em_dic<-read_xlsx("item_dic.xlsx",1)
+eem_bd=read_xlsx("EMS - Banco integrado Coleta 2016.xlsx",1)
+
+eem_dic<-read_xlsx("item_dic.xlsx",1)
 
 #análises de consistência interna
-em_bd%>%
+eem_bd%>%
   select(starts_with("EMS_"))%>%
   alpha(check.keys=TRUE)
 
-em_bd%>%
+egoista=eem_dic%>%filter(factor=="egoistic")%>%
+  select(item_cod)%>%pull()
+
+moralista=eem_dic%>%filter(factor=="moralistic")%>%
+  select(item_cod)%>%pull()
+
+eem_bd%>%
+  select(all_of(egoista))%>%
+  alpha(check.keys=TRUE)
+
+eem_bd%>%
+  select(all_of(moralista))%>%
+  alpha(check.keys=TRUE)
+
+
+eem_bd%>%
   select(starts_with("EMS_"))%>%
   omega()
+
+#modo "à manivela" de selecionar itens
+alpha(eem_bd[,c(2:8)])
